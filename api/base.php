44c6974837ee066238e929fc3d0f29f1cd8325a0
FY Kuo <teresa99339@gmail.com>
2022-09-12 12:52:52 +0800

    前台院線片清單顯示完成 詳細電影介紹完成

--- api/base.php
+++ api/base.php
@@ -33,7 +33,7 @@ class DB
         if(isset($arg[1])){
             $sql .= $arg[1];
         }
-
+        // echo $sql;
         return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
     }
 
--- front/main.php
+++ front/main.php
@@ -35,17 +35,87 @@
         </div>
     </div>
 </div>
+
+
 <div class="half">
     <h1>院線片清單</h1>
     <div class="rb tab" style="width:95%;">
-        <table>
-            <tbody>
-                <tr> </tr>
-            </tbody>
-        </table>
-        <div class="ct"> </div>
+        
+        <div class="d-f f-w">
+            <?php
+            $endDay = date("Y-m-d",strtotime("-2 days"));
+            $num = $Movie->math('COUNT','id'," WHERE `sh` = 1 AND `date` BETWEEN '$endDay' AND '$today' ORDER BY `rank` ");
+            $limit = 4;
+            $pages = ceil($num/$limit);
+            $page = ($_GET['page'])??1;
+            if($page <= 0 || $page > $pages){
+                $page = 1;
+            }
+            $start = ($page-1)*$limit;
+            $movies = $Movie->all(" WHERE `sh` = 1 AND `date` BETWEEN '$endDay' AND '$today' ORDER BY `rank` Limit $start,$limit");
+
+            foreach ($movies as $key => $movie) {
+                switch ($movie['type']) {
+                    case 1:
+                        $type = '普遍級';
+                    break;
+                    case 2:
+                        $type = '保護級';
+                    break;
+                    case 3:
+                        $type = '輔導級';
+                    break;
+                    case 4:
+                        $type = '限制級';
+                    break;
+                    
+                }
+            ?>
+            <div class="w-45 d-f a-c fs-12 f-w p-10">
+                <div class="w-50 d-f a-c j-c">
+                    <img src="./img/<?=$movie['img']?>" alt="" width="80px">
+                </div>
+                <div class="w-50">
+                    <div class="my-5"><?=$movie['name']?></div>
+                    <div class="my-5">分級：<img src="./icon/03C0<?=$movie['type']?>.png" alt="" width="15px"><?=$type?></div>
+                    <div class="my-5">上映日期：<?=$movie['date']?></div>
+                </div>
+                <div class="w-100 ct my-10">
+                    <input type="button" value="劇情簡介" onclick="to('?do=intro&id=<?=$movie['id']?>')">
+                    <input type="button" value="線上訂票" onclick="to('?do=order&id=<?=$movie['id']?>')">
+                </div>
+            </div>
+
+            <?php
+            }
+            ?>
+
+        </div>
+        
+        <div class="ct page">
+            <?php
+            if($page > 1){
+            ?>
+            <a href="?page=<?=$page-1?>">&lt;</a>
+            <?php
+            }
+            for ($i=1; $i <= $pages ; $i++) { 
+            ?>
+            <a href="?page=<?=$i?>" class="<?=($page == $i)?'nowPage':''?>"><?=$i?></a>
+            <?php
+            }
+            if($page < $pages){
+            ?>
+            <a href="?page=<?=$page+1?>">&gt;</a>
+            <?php
+            }
+            ?>
+        </div>
     </div>
 </div>
+
+
+
 
 <script>
     $('.poster_item').eq(0).show();
